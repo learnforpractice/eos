e793b604d8dbc7bf1f572607fbeae975af622ffd
learnforpractice <learnforpractice@gmail.com>
2019-09-03 09:01:42 +0800

    Add more testing code

---
+++ unittests/pythonvm_tests.cpp
@@ -0,0 +1,43 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#include <eosio/chain/abi_serializer.hpp>
+#include <eosio/chain/resource_limits.hpp>
+#include <eosio/chain/generated_transaction_object.hpp>
+#include <eosio/testing/tester.hpp>
+
+#include <Runtime/Runtime.h>
+
+#include <fc/variant_object.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+#include <contracts.hpp>
+
+#include "fork_test_utilities.hpp"
+
+using namespace eosio::chain;
+using namespace eosio::testing;
+
+BOOST_AUTO_TEST_SUITE(pythonvm_tests)
+
+BOOST_AUTO_TEST_CASE( test1 ) try {
+   tester c( setup_policy::preactivate_feature_and_new_bios );
+
+   c.create_accounts( {N(alice), N(bob), N(charlie)} );
+   c.produce_block();
+   vector<uint8_t> v;
+   v.resize(1);
+   v.data()[0] = 0; 
+   c.set_code( N(bob), v, 1);
+{
+   vector<uint8_t> d;
+   auto r = c.push_action( N(bob), N(testcreate), d, N(bob));
+   ilog("{console}", ("console", r->action_traces[0].console));
+   c.produce_block();
+}
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_AUTO_TEST_SUITE_END()
