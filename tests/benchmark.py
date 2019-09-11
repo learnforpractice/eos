655ce577e12c6f54b7016b34f670b3861617ddf1
learnforpractice <learnforpractice@gmail.com>
2019-09-11 22:04:22 +0800

    Update benchmark.py

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -8,6 +8,7 @@ from pyeoskit import wallet
 from pyeoskit import eosapi
 from pyeoskit import config
 from pyeoskit import db
+from pyeoskit import util
 
 from pyeoskit.exceptions import HttpAPIError
 import sys
@@ -39,18 +40,19 @@ key2 = 'EOS7ent7keWbVgvptfYaMYeF2cenMBiwYKcwEuc11uCbStsFKsrmV'
 
 def create_system_accounts():
     systemAccounts = [
-        'eosio.token',
         'eosio.bpay',
         'eosio.msig',
         'eosio.names',
         'eosio.ram',
         'eosio.ramfee',
-        'eosio.jit',
-        'eosio.jitfee',
         'eosio.saving',
         'eosio.stake',
+        'eosio.token',
         'eosio.vpay',
         'eosio.rex',
+
+        'eosio.jit',
+        'eosio.jitfee',
         'uuos',
         'hello',
         'helloworld12',
@@ -81,6 +83,11 @@ def create_system_accounts():
 
 create_system_accounts()
 
+key = 'EOS5yprUeBro1asg4r1b4BKuJViy6wwyJmXiP4Zqzu1Z4ixpPwuLX'
+account_name = 'uuos.fund' #'uuosuuos1.m'
+if not eosapi.get_account(account_name):
+    eosapi.create_account('eosio', account_name, key, key)
+
 
 contract_path = '/Users/newworld/dev/eos/build/contracts'
 contract_path = '/Users/newworld/dev/uuos2/build/externals/eosio.contracts/contracts'
@@ -102,17 +109,19 @@ if 1:
     code_hash = m.hexdigest()
     try:
         r = eosapi.get_code('eosio.token')
+        print(code_hash, r['code_hash'])
         if code_hash != r['code_hash']:
+            print("++++++++++set eosio.token contract")
             r = eosapi.set_contract('eosio.token', code, abi, 0)
     except Exception as e:
         r = eosapi.set_contract('eosio.token', code, abi, 0)
 
     if not eosapi.get_balance('eosio'):
         print('issue system token...')
-        msg = {"issuer":"eosio","maximum_supply":f"11000000000000.0000 {config.main_token}"}
+        msg = {"issuer":"eosio","maximum_supply":f"11000000000.0000 {config.main_token}"}
         r = eosapi.push_action('eosio.token', 'create', msg, {'eosio.token':'active'})
         assert r
-        r = eosapi.push_action('eosio.token','issue',{"to":"eosio","quantity":f"10000000000000.0000 {config.main_token}","memo":""},{'eosio':'active'})
+        r = eosapi.push_action('eosio.token','issue',{"to":"eosio","quantity":f"1000000000.0000 {config.main_token}","memo":""},{'eosio':'active'})
         assert r
 
 code_path = os.path.join(contract_path, 'eosio.msig/eosio.msig.wasm')
@@ -128,8 +137,8 @@ r = eosapi.get_code('eosio.msig')
 if code_hash != r['code_hash']:
     r = eosapi.set_contract('eosio.msig', code, abi, 0)
 try:
-    eosapi.schedule_protocol_feature_activations(['0ec7e080177b2c02b278d5088611686b49d739925a92d9bfcacd7fc6b74053bd'])
-    time.sleep(1.0)
+    eosapi.schedule_protocol_feature_activations(['0ec7e080177b2c02b278d5088611686b49d739925a92d9bfcacd7fc6b74053bd']) #PREACTIVATE_FEATURE
+    time.sleep(2.0)
 except Exception as e:
     print(e)
 
@@ -144,16 +153,47 @@ m.update(code)
 code_hash = m.hexdigest()
 r = eosapi.get_code('eosio')
 if code_hash != r['code_hash']:
+    print('publish system contract...')
     r = eosapi.set_contract('eosio', code, abi, 0)
 
 
-args = {'version':0, 'core':'4,UUOS'}
-eosapi.push_action('eosio', 'init', args, {'eosio':'active'})
+try:
+    args = {'version':0, 'core':'4,UUOS'}
+    eosapi.push_action('eosio', 'init', args, {'eosio':'active'})
+except Exception as e:
+    print(e)
 
 #r = eosapi.push_action('eosio.token', 'issue', {"to":"uuos","quantity":f"1000.0000 {config.main_token}","memo":""}, {'eosio':'active'})
 #print(r)
 r = eosapi.push_action('eosio.token', 'transfer', {"from":"eosio", "to":"uuos","quantity":f"1000.0000 {config.main_token}","memo":""}, {'eosio':'active'})
 
+r = eosapi.push_action('eosio.token', 'transfer', {"from":"eosio", "to":"helloworld11","quantity":f"10000000.0000 {config.main_token}","memo":""}, {'eosio':'active'})
+
+#eosapi.schedule_protocol_feature_activations(['ad9e3d8f650687709fd68f4b90b41f7d825a365b02c23a636cef88ac2ac00c43']) #RESTRICT_ACTION_TO_SELF
+#eosapi.schedule_protocol_feature_activations(['4a90c00d55454dc5b059055ca213579c6ea856967712a56017487886a4d4cc0f']) #NO_DUPLICATE_DEFERRED_ID
+
+feature_digests = ['ad9e3d8f650687709fd68f4b90b41f7d825a365b02c23a636cef88ac2ac00c43',#RESTRICT_ACTION_TO_SELF
+            'ef43112c6543b88db2283a2e077278c315ae2c84719a8b25f25cc88565fbea99',#REPLACE_DEFERRED
+            '4a90c00d55454dc5b059055ca213579c6ea856967712a56017487886a4d4cc0f'#NO_DUPLICATE_DEFERRED_ID
+]
+for digest in feature_digests: 
+    try:
+        args = {'feature_digest': 'ad9e3d8f650687709fd68f4b90b41f7d825a365b02c23a636cef88ac2ac00c43'} #RESTRICT_ACTION_TO_SELF
+        eosapi.push_action('eosio', 'activate', args, {'eosio':'active'})
+    except Exception as e:
+        print(e)
+
+from datetime import datetime
+data_string = "2019-10-24 08:00:00" #timezone: +8
+dt = datetime.fromisoformat(data_string)
+print("+++++++++=main net start time:", time.mktime(dt.timetuple()))
+
+
+key = 'EOS5yprUeBro1asg4r1b4BKuJViy6wwyJmXiP4Zqzu1Z4ixpPwuLX'
+account_name = 'uuos.fund' #'uuosuuos1.m'
+seed_round_found = 100000000.0000
+util.buyrambytes('eosio', account_name, 10*1024)
+util.dbw('eosio', account_name, seed_round_found*0.1, seed_round_found*0.9, transfer=True)
 
 balance = eosapi.get_balance('uuos')
 print('++++balance: ', balance)
