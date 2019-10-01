7144052a3dff26b31fe6bbd94db4e36cd00698ff
learnforpractice <learnforpractice@gmail.com>
2019-10-01 10:23:56 +0800

    move some native code to main.cpp

--- contracts/native/eosio.token/main.cpp
+++ contracts/native/eosio.token/main.cpp
@@ -1,11 +1,22 @@
-typedef unsigned __int128 uint128_t;
 
-#include <stdint.h>
-#include <eosio/db.h>
-#include <eosio/multi_index.hpp>
+#include <eosio.token/eosio.token.hpp>
 
-extern "C" void apply(uint64_t receiver, uint64_t first_receiver, uint64_t action);
-void native_apply(uint64_t receiver, uint64_t first_receiver, uint64_t action) {
-    apply(receiver, first_receiver, action);
+#define EOSIO_DISPATCH_NATIVE( TYPE, MEMBERS ) \
+extern "C" { \
+   __attribute__ ((visibility ("default"))) void eosio_token_apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
+      if( code == receiver ) { \
+         switch( action ) { \
+            EOSIO_DISPATCH_HELPER( TYPE, MEMBERS ) \
+         } \
+         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
+      } \
+   } \
 }
+
+#ifdef _NATIVE
+EOSIO_DISPATCH_NATIVE( eosio::token, (create)(issue)(transfer)(open)(retire) )
+#endif
+
+EOSIO_DISPATCH( eosio::token, (create)(issue)(transfer)(open)(retire) )
+
 
