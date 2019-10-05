7bd8f295d9358c3360bf1fbb5ce14045cf4c943e
learnforpractice <learnforpractice@gmail.com>
2019-10-05 21:01:29 +0800

    add boost as a submodule

--- CMakeModules/fc/CMakeModules/FindBoost.cmake
+++ CMakeModules/fc/CMakeModules/FindBoost.cmake
@@ -1,4 +1,4 @@
 message(STATUS "Using custom FindBoost.cmake")
 set(Boost_FOUND TRUE)
-set(Boost_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/externals/boost_1_67_0/includes")
+set(Boost_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/externals/boost/includes")
 set(Boost_LIBRARIES boost_1_67_0)
