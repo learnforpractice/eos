caf72e7bb1bb48a30520340823a80f9bb07e82a2
learnforpractice <learnforpractice@gmail.com>
2019-10-16 09:46:47 +0800

    Updte mi testcase

--- unittests/test-contracts/mi_test/main.cpp
+++ unittests/test-contracts/mi_test/main.cpp
@@ -23,9 +23,12 @@ extern "C" void apply(uint64_t receiver, uint64_t code, uint64_t action) {
    uint64_t table = "table"_n.value;
    multi_index mi(code, scope, table, index_types);
 
+//test store
+{
    vector<vector<char>> secondary_values;
    uint64_t primary_key = "hello"_n.value;
    uint64_t secondary_key = "world"_n.value;
+
 
    vector<char> secondary_value((char*)&secondary_key, (char*)&secondary_key + 8);
    secondary_values.push_back(secondary_value);
@@ -33,7 +36,30 @@ extern "C" void apply(uint64_t receiver, uint64_t code, uint64_t action) {
 
    uint64_t primary_key2;
    int itr = mi.idx_find(0, primary_key2, &secondary_key, sizeof(secondary_key));
-   check(primary_key == primary_key2, "bad value");
+   check(primary_key == primary_key2, "bad secondary");
+}
+
+//test modify
+{
+   vector<vector<char>> secondary_values;
+   uint64_t primary_key = "hello"_n.value;
+   uint64_t secondary_key = "earth"_n.value;
+
+   vector<char> secondary_value((char*)&secondary_key, (char*)&secondary_key + 8);
+   secondary_values.push_back(secondary_value);
+   int itr = mi.find(primary_key);
+   mi.modify(itr, primary_key, (void *)"goodbye", 7, secondary_values, payer);
+
+   char buffer[256];
+   int data_size = mi.get(itr, buffer, 256);
+   check(memcmp(buffer, "goodbye", 7) == 0, "bad data");
+
+   uint64_t primary_key2;
+   itr = mi.idx_find(0, primary_key2, &secondary_key, sizeof(secondary_key));
+   check(primary_key == primary_key2, "bad secondary");
+}
+
+
 
 }
 
