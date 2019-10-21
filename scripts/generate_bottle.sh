bb2bd2718cc7caeb3e43bbd1eaddb357402a3ee0
learnforpractice <learnforpractice@gmail.com>
2019-10-21 10:29:16 +0800

    Various changes

--- scripts/generate_bottle.sh
+++ scripts/generate_bottle.sh
@@ -8,7 +8,7 @@ then
    if [[ -z "$VERS" ]];
    then
       echo "Error, unsupported OS X version"
-      exit -1
+#      exit -1
    fi
    MAC_VERSION="mojave"
 else
@@ -17,7 +17,7 @@ fi
 
 NAME="${PROJECT}-${VERSION}.${MAC_VERSION}.bottle"
 
-mkdir -p ${PROJECT}/${VERSION}/opt/eosio/lib/cmake
+mkdir -p ${PROJECT}/${VERSION}/opt/uuosio/lib/cmake
 
 PREFIX="${PROJECT}/${VERSION}"
 SPREFIX="\/usr\/local"
@@ -33,11 +33,11 @@ export SSUBPREFIX
 
 hash=`openssl dgst -sha256 ${NAME}.tar.gz | awk 'NF>1{print $NF}'`
 
-echo "class Eosio < Formula
+echo "class Uuosio < Formula
 
    homepage \"${URL}\"
    revision 0
-   url \"https://github.com/eosio/eos/archive/v${VERSION}.tar.gz\"
+   url \"https://github.com/uuosio/uuos/archive/v${VERSION}.tar.gz\"
    version \"${VERSION}\"
 
    option :universal
@@ -50,13 +50,13 @@ echo "class Eosio < Formula
    depends_on :arch =>  :intel
 
    bottle do
-      root_url \"https://github.com/eosio/eos/releases/download/v${VERSION}\"
+      root_url \"https://github.com/uuosio/uuos/releases/download/v${VERSION}\"
       sha256 \"${hash}\" => :${MAC_VERSION}
    end
    def install
       raise \"Error, only supporting binary packages at this time\"
    end
 end
-__END__" &> eosio.rb
+__END__" &> uuosio.rb
 
 rm -r ${PROJECT} || exit 1
