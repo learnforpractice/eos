3ad6663cedc99aeb6b9ed9c87b547e45f7904349
learnforpractice <learnforpractice@gmail.com>
2019-10-21 09:37:14 +0800

    Update package info

--- CMakeModules/package.cmake
+++ CMakeModules/package.cmake
@@ -1,8 +1,8 @@
-set(VENDOR "block.one")
-set(PROJECT_NAME "eosio")
-set(DESC "Software for the EOS.IO network")
-set(URL "https://github.com/eosio/eos")
-set(EMAIL "support@block.one")
+set(VENDOR "uuos.io")
+set(PROJECT_NAME "uuosio")
+set(DESC "Software for the uuos.io network")
+set(URL "https://github.com/uuosio/uuos")
+set(EMAIL "uuosone@gmail.com")
 
 configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_package.sh.in ${CMAKE_BINARY_DIR}/packages/generate_package.sh @ONLY)
 configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_bottle.sh ${CMAKE_BINARY_DIR}/packages/generate_bottle.sh COPYONLY)
