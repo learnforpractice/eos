b185015b7cf47a80ee9620a8406d1c46ed00ba0c
learnforpractice <learnforpractice@gmail.com>
2019-12-01 15:39:21 +0800

    Updates

--- programs/hello/src/_hello.pyx
+++ programs/hello/src/_hello.pyx
@@ -9,12 +9,14 @@ from libcpp cimport bool
 #cdef extern from <stdint.h>:
     
 
-cdef extern from "cpp_object.hpp":
+cdef extern from "native_object.hpp":
     void pack_native_object_(int _type, string& msg, string& packed_message)
     void unpack_native_object_(int _type, string& packed_message, string& msg)
 
     void *chain_new_(string& config, string& protocol_features_dir)
     void chain_free_(void *ptr)
+
+    void chain_on_incoming_block_(void *ptr, string& packed_signed_block)
 
 cpdef void hello(str strArg):
     "Prints back 'Hello <param>', for example example: hello.hello('you')"
@@ -40,3 +42,6 @@ def chain_new(string& config, string& protocol_features_dir):
 
 def chain_free(unsigned long long  ptr):
     chain_free_(<void *>ptr);
+
+def chain_on_incoming_block(unsigned long long ptr, string& packed_signed_block):
+    chain_on_incoming_block_(<void *>ptr, packed_signed_block)
