137c3ca7a3da2f5bbd85012d3295f14ee4848126
learnforpractice <learnforpractice@gmail.com>
2019-12-02 02:47:10 +0800

    Update main.py

--- programs/hello/main.py
+++ programs/hello/main.py
@@ -1,8 +1,11 @@
 from _hello import *
 
 import os
+import io
+import time
 import sys
-import ujson
+#import ujson as json
+import json
 import struct
 import logging
 import asyncio
@@ -79,6 +82,19 @@ logger.addHandler(logging.StreamHandler())
 
 app = App(__name__)
 
+chain_ptr = None
+def init():
+    cfg = ControllerConfig(default_config)
+    # "blocks_dir": "/Users/newworld/dev/uuos2/build/programs/dd/blocks",
+    # "state_dir": "/Users/newworld/dev/uuos2/build/programs/dd/state",
+    cfg.blocks_dir = 'dd/blocks'
+    cfg.state_dir = 'dd/state'
+    cfg = cfg.dumps()
+    ptr = chain_new(cfg, 'cd')
+    print(ptr)
+    return ptr
+    # chain_free(ptr)
+
 @app.route('/')
 async def hello():
     return 'hello'
@@ -94,9 +110,13 @@ async def p2p_client(host, port):
     reader, writer = await asyncio.open_connection(host, port)
     logger.info(f'connected to {host}:{port} success!')
 
-    msg = HandshakeMessage(handshake_msg)
+    msg = HandshakeMessage(default_handshake_msg)
     msg.network_version = 1206
-    msg.chain_id = 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f'
+    msg.chain_id = 'e1b12a9d0720401efa34556d4cb80f0f95c3d0a3a913e5470e8ea9ff44719381'
+    msg.last_irreversible_block_num = 3
+    msg.last_irreversible_block_id = "0000000363dabcdeb154ad6376bfcc6d95985e07592fd4037c992a35a0a1405d"
+    msg.head_num = 3
+    msg.head_id = "0000000363dabcdeb154ad6376bfcc6d95985e07592fd4037c992a35a0a1405d"
     msg = msg.pack()
     writer.write(msg)
     try:
@@ -112,25 +132,67 @@ async def p2p_client(host, port):
         logger.exception(e)
         return
 
-    data = struct.pack('IB', 8+1, sync_request_message_type) + struct.pack('II', 1, 5)
+    data = struct.pack('IB', 8+1, sync_request_message_type) + struct.pack('II', 1, 100000)
     writer.write(data)
+    count = 0
+    block_file = open('block.bin', 'wb')
     while True:
         msg_len = await reader.read(4)
+        block_file.write(msg_len)
         msg_len = int.from_bytes(msg_len, 'little')
-        print('++++read:', msg_len)
-        msg = await reader.read(msg_len)
-        print(msg[0], msg)
-        if msg[0] == 3:
+        read_len = 0
+        msg = b'' #io.BytesIO()
+        while True:
+            remained = await reader.read(msg_len-read_len)
+            msg += remained
+#            msg.write(remained)
+            read_len += len(remained)
+            if read_len >= msg_len:
+                break
+#        msg = msg.getvalue()
+#        block_file.write(msg)
+#        block_file.flush()
+        count += 1
+#        print(' ++count:', count)
+        if count % 10000 == 0:
+             print(' ++count:', count)
+#        print('++++read:', msg_len, len(msg), ' ++count:', count,  msg[0], msg)
+        if msg[0] == handshake_message_type:
+            print(count, msg[0], msg)
+            logger.info('bad handshake_message')
+            # msg = HandshakeMessage.unpack(msg[1:])
+            # print(msg)
+        elif msg[0] == 3:
             msg = TimeMessage.unpack(msg[1:])
             logger.info(msg)
+            data = struct.pack('IB', 8+1, sync_request_message_type) + struct.pack('II', 1, 2)
+#            writer.write(data)
+#            block_file.close()
         elif msg[0] == 4:
             msg = NoticeMessage.unpack(msg[1:])
             print(msg)
         elif msg[0] == 7:
-            msg = SignedBlockMessage.unpack(msg[1:])
-            logger.info(msg)
+            msg = msg[1:]
+            continue
+#            block_file.write(msg)
+            block = SignedBlockMessage.unpack(msg)
+            if not block:
+                logger.info('bad block')
+                return
+            block_num = bytes.fromhex(block.previous[:8])
+            block_num = int.from_bytes(block_num, 'big')
+#            print('++++block_num:', block_num)
+            # print(block)
+            # logger.info(f'++++block num {block_num}')
+            # logger.info(block)
+
+#            00000c44a59a5c18ce72c1540407cadc91f03d890befe993cde8df3e508ce024
+#            chain_on_incoming_block(chain_ptr, msg)
 
 async def uuos_main(args):
+    global chain_ptr
+    chain_ptr = init()
+    logger.info(f"++++++chain_ptr:{chain_ptr}")
     for address in args.p2p_peer_address:
         host, port = address.split(':')
         await p2p_client(host, port)
@@ -151,6 +213,8 @@ async def main(args):
     return res
 
 if __name__ == "__main__":
+    print(os.getpid())
+#    time.sleep(10)
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--data-dir',               type=str, default='',                  help='data directory')
     parser.add_argument('--config-dir',             type=str, default='',                  help='config directory')
@@ -160,5 +224,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 #    print(args.data_dir, args.config_dir, args.http_server_address, args.p2p_listen_endpoint)
     print(args.p2p_peer_address)
-    asyncio.run(main(args))
-
+    try:
+        asyncio.run(main(args))
+    except KeyboardInterrupt:
+        logger.info("Process interrupted") 
+    if chain_ptr:
+        chain_free(chain_ptr)
