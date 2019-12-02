584ca80efdb764bd6b7484a06bcca48f8a99dde3
learnforpractice <learnforpractice@gmail.com>
2019-12-02 17:42:38 +0800

    Refactoring code

--- programs/hello/main.py
+++ programs/hello/main.py
@@ -2,83 +2,19 @@ import os
 import io
 import time
 import sys
-#import ujson as json
-import json
+import ujson as json
 import struct
 import logging
 import asyncio
 import argparse
-from quart import Quart, websocket
-from quart.logging import create_logger, create_serving_logger
 
-from hypercorn.asyncio import serve
-from hypercorn.config import Config as HyperConfig
-
-from typing import (
-    Any,
-    AnyStr,
-    Awaitable,
-    Callable,
-    cast,
-    Dict,
-    IO,
-    Iterable,
-    List,
-    Optional,
-    Set,
-    Tuple,
-    Type,
-    Union,
-    ValuesView,
-)
-
-from pyeoskit import eosapi
+from uuos.rpc_server import rpc_server
 from native_object import *
-
-class App(Quart):
-    def server(
-        self,
-        host: str = "127.0.0.1",
-        port: int = 5000,
-        debug: Optional[bool] = None,
-        use_reloader: bool = True,
-        loop: Optional[asyncio.AbstractEventLoop] = None,
-        ca_certs: Optional[str] = None,
-        certfile: Optional[str] = None,
-        keyfile: Optional[str] = None,
-    ) -> None:
-
-        config = HyperConfig()
-        config.access_log_format = "%(h)s %(r)s %(s)s %(b)s %(D)s"
-        config.accesslog = create_serving_logger()
-        config.bind = [f"{host}:{port}"]
-        config.ca_certs = ca_certs
-        config.certfile = certfile
-        if debug is not None:
-            self.debug = debug
-        config.errorlog = config.accesslog
-        config.keyfile = keyfile
-        config.use_reloader = use_reloader
-
-        scheme = "https" if config.ssl_enabled else "http"
-        print(  # noqa: T001, T002
-            "Running on {}://{} (CTRL + C to quit)".format(scheme, config.bind[0])
-        )
-        
-        return serve(self, config)
-
-        if loop is not None:
-            loop.set_debug(debug or False)
-            loop.run_until_complete(serve(self, config))
-        else:
-            asyncio.run(serve(self, config), debug=config.debug)
 
 logging.basicConfig(filename='logfile.log', level=logging.INFO, 
                     format='%(asctime)s %(levelname)s %(module)s %(lineno)d %(message)s')
 logger=logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
-
-app = App(__name__)
 
 chain_ptr = None
 def init():
@@ -87,7 +23,7 @@ def init():
     # "state_dir": "/Users/newworld/dev/uuos2/build/programs/dd/state",
     cfg.blocks_dir = 'dd/blocks'
     cfg.state_dir = 'dd/state'
-    cfg.genesis = genesis_uuos
+#    cfg.genesis = genesis_uuos
 
     cfg = cfg.dumps()
     ptr = chain_new(cfg, 'cd')
@@ -95,14 +31,6 @@ def init():
     return ptr
     # chain_free(ptr)
 
-@app.route('/')
-async def hello():
-    return 'hello'
-
-@app.websocket('/ws')
-async def ws():
-    while True:
-        await websocket.send('hello')
 handshake = 'b604cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f122f727370ec82744c546a3c7e17c508fb3f8f6acd76fb78f91b8efaa531c1b60000000000000000000000000000000000000000000000000000000000000000000098709fe00198db150000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000183132372e302e302e313a39383737202d2031323266373237c2160000000016c2769ea3daa77be00b2613af499d01007a1cb01d2b020493526cfb0115c3160000000016c39195db0558624e6b50fa26bc2342c45c063a2d3472a31df6324ae29d036f73781022454f532054657374204167656e74220100'
 handshake = bytes.fromhex(handshake)
 
@@ -146,11 +74,11 @@ async def p2p_client(host, port):
 
     msg = HandshakeMessage(default_handshake_msg)
     msg.network_version = 1206
-    msg.chain_id = 'e1b12a9d0720401efa34556d4cb80f0f95c3d0a3a913e5470e8ea9ff44719381'
-    msg.last_irreversible_block_num = 3
-    msg.last_irreversible_block_id = "0000000363dabcdeb154ad6376bfcc6d95985e07592fd4037c992a35a0a1405d"
-    msg.head_num = 3
-    msg.head_id = "0000000363dabcdeb154ad6376bfcc6d95985e07592fd4037c992a35a0a1405d"
+    # msg.chain_id = 'e1b12a9d0720401efa34556d4cb80f0f95c3d0a3a913e5470e8ea9ff44719381'
+    # msg.last_irreversible_block_num = 3
+    # msg.last_irreversible_block_id = "0000000363dabcdeb154ad6376bfcc6d95985e07592fd4037c992a35a0a1405d"
+    # msg.head_num = 3
+    # msg.head_id = "0000000363dabcdeb154ad6376bfcc6d95985e07592fd4037c992a35a0a1405d"
     print(msg)
 
     msg = msg.pack()
@@ -169,7 +97,7 @@ async def p2p_client(host, port):
         logger.exception(e)
         return
 
-    data = struct.pack('IB', 8+1, sync_request_message_type) + struct.pack('II', 1, 100000)
+    data = struct.pack('IB', 8+1, sync_request_message_type) + struct.pack('II', 1, 1000000)
     writer.write(data)
     count = 0
 #    block_file = open('block.bin', 'wb')
@@ -181,6 +109,8 @@ async def p2p_client(host, port):
             return
         msg = await read(reader, msg_len)
         count += 1
+        if count % 1 == 0:
+            print('+++count:', count)
         if msg[0] == handshake_message_type:
             print(count, msg[0], len(msg), msg.hex())
             logger.info('bad handshake_message')
@@ -194,15 +124,16 @@ async def p2p_client(host, port):
             print(msg)
         elif msg[0] == 7:
             msg = msg[1:]
-            block = SignedBlockMessage.unpack(msg)
-            if not block:
-                continue
-                logger.info('bad block')
-                continue
-            block_num = bytes.fromhex(block.previous[:8])
-            block_num = int.from_bytes(block_num, 'big')
-            if block_num % 10000 == 0:
-                print('++++block_num:', block_num)
+            if False:
+                block = SignedBlockMessage.unpack(msg)
+                if not block:
+                    continue
+                    logger.info('bad block')
+                    continue
+                block_num = bytes.fromhex(block.previous[:8])
+                block_num = int.from_bytes(block_num, 'big')
+                if block_num % 1 == 0:
+                    print('++++block_num:', block_num)
             # print(block)
             # logger.info(f'++++block num {block_num}')
             # logger.info(block)
@@ -220,14 +151,15 @@ async def uuos_main(args):
         print('hello')
 
 async def main(args):
-    try:
-        host, port = args.http_server_address.split(':')
-        port = int(port)
-    except Exception as e:
-        logger.exception(e)
-        return
+    tasks = []
+    task = asyncio.create_task(rpc_server(args))
+    tasks.append(task)
+    
+    task = asyncio.create_task(uuos_main(args))
+    tasks.append(task)
+
 #    res = await asyncio.gather(uuos_main(args), app.server(host=host, port=port), return_exceptions=True)
-    res = await asyncio.gather(uuos_main(args), return_exceptions=False)
+    res = await asyncio.gather(*tasks, return_exceptions=False)
     print(res)
     return res
 
@@ -243,9 +175,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 #    print(args.data_dir, args.config_dir, args.http_server_address, args.p2p_listen_endpoint)
     print(args.p2p_peer_address)
+
     try:
-        asyncio.run(main(args))
+        loop = asyncio.get_event_loop()
+        args.loop = loop
+        loop.run_until_complete(main(args))
+        # asyncio.run(main(args))
     except KeyboardInterrupt:
-        logger.info("Process interrupted") 
+        logger.info("Processing interrupted")
     if chain_ptr:
         chain_free(chain_ptr)
---
+++ programs/hello/uuos/rpc_server.py
@@ -0,0 +1,96 @@
+import os
+import io
+import time
+import sys
+#import ujson as json
+import json
+import struct
+import logging
+import asyncio
+import argparse
+from quart import Quart, websocket
+from quart.logging import create_logger, create_serving_logger
+
+from hypercorn.asyncio import serve
+from hypercorn.config import Config as HyperConfig
+from pyeoskit import eosapi
+
+from typing import (
+    Any,
+    AnyStr,
+    Awaitable,
+    Callable,
+    cast,
+    Dict,
+    IO,
+    Iterable,
+    List,
+    Optional,
+    Set,
+    Tuple,
+    Type,
+    Union,
+    ValuesView,
+)
+
+class App(Quart):
+    def server(
+        self,
+        host: str = "127.0.0.1",
+        port: int = 5000,
+        debug: Optional[bool] = None,
+        use_reloader: bool = True,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+        ca_certs: Optional[str] = None,
+        certfile: Optional[str] = None,
+        keyfile: Optional[str] = None,
+    ) -> None:
+
+        config = HyperConfig()
+        config.access_log_format = "%(h)s %(r)s %(s)s %(b)s %(D)s"
+        config.accesslog = create_serving_logger()
+        config.bind = [f"{host}:{port}"]
+        config.ca_certs = ca_certs
+        config.certfile = certfile
+        if debug is not None:
+            self.debug = debug
+        config.errorlog = config.accesslog
+        config.keyfile = keyfile
+        config.use_reloader = use_reloader
+
+        scheme = "https" if config.ssl_enabled else "http"
+        print(  # noqa: T001, T002
+            "Running on {}://{} (CTRL + C to quit)".format(scheme, config.bind[0])
+        )
+        
+        return serve(self, config)
+
+        if loop is not None:
+            loop.set_debug(debug or False)
+            loop.run_until_complete(serve(self, config))
+        else:
+            asyncio.run(serve(self, config), debug=config.debug)
+
+logger=logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+app = App(__name__)
+
+@app.route('/')
+async def hello():
+    return 'hello'
+
+@app.websocket('/ws')
+async def ws():
+    while True:
+        await websocket.send('hello')
+
+
+async def rpc_server(args):
+    try:
+        host, port = args.http_server_address.split(':')
+        port = int(port)
+        await app.server(host=host, port=port, loop=args.loop, use_reloader=False)
+    except Exception as e:
+        logger.exception(e)
+        return
