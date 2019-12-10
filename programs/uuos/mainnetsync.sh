e98229e5ecb220a1d1269307312fc7cbf0354a46
learnforpractice <learnforpractice@gmail.com>
2019-12-10 09:56:48 +0800

    Add mainnetsync.sh

---
+++ programs/uuos/mainnetsync.sh
@@ -0,0 +1,2 @@
+python3 main.py --data-dir dd2 --config-dir cd2 --http-server-address 127.0.0.1:8890 --p2p-listen-endpoint 127.0.0.1:9881 --p2p-peer-address peer.main.alohaeos.com:9876 --network eos
+
