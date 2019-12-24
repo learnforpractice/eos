08bc7e8bc4822b713dcac0b3dfd594b550844596
learnforpractice <learnforpractice@gmail.com>
2019-12-24 04:16:03 -0800

    Update run.sh

--- programs/uuos/run.sh
+++ programs/uuos/run.sh
@@ -1,2 +1,2 @@
-python3 main.py --data-dir dd --config-dir cd --p2p-peer-address 127.0.0.1:9876  --http-server-address 127.0.0.1:8889
+python3.7 main.py --data-dir dd --config-dir cd --p2p-peer-address 127.0.0.1:9876  --http-server-address 127.0.0.1:8889
 
