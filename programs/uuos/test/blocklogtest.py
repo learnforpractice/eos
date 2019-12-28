d0ffc2ed22ce17edcdc4da639c06fd0979861c68
learnforpractice <learnforpractice@gmail.com>
2019-12-28 20:02:37 +0800

    Update blocklog

--- programs/uuos/test/blocklogtest.py
+++ programs/uuos/test/blocklogtest.py
@@ -10,5 +10,17 @@ from uuos.blocklog import BlockLog
 if __name__ == '__main__':
     log = BlockLog('dd-eos/blocks')
     # count = log.get_block_count()
-    print(log.get_first_block_num(), log.get_head_block_num())
+    print(log.first_block_num, log.get_head_block_num())
+    block = log.read_block_by_num(log.first_block_num)
+    print(block['previous'])
 
+    previous = block['previous'][:8]
+    previous = bytes.fromhex(previous)
+    previous = int.from_bytes(previous, 'big')
+    print(previous)
+
+    block = log.read_block_by_num(log.first_block_num+1)
+    print(block)
+    
+    block = log.read_block_by_id(block['previous'])
+    print(block)
--- programs/uuos/uuos/blocklog.py
+++ programs/uuos/uuos/blocklog.py
@@ -1,6 +1,7 @@
 import io
 import os
 import struct
+import ujson as json
 from _uuos import unpack_native_object
 
 min_supported_version = 1
@@ -22,26 +23,26 @@ class BlockLog(object):
                 f"Unsupported version of block log. Block log version is {self.version} while code supports version(s) [{min_supported_version},{max_supported_version}]"
 
         if self.version > 1:
-            self.first_block_num = self.fd.read(4)
-            self.first_block_num, = struct.unpack('I', self.first_block_num)
+            self._first_block_num = self.fd.read(4)
+            self._first_block_num, = struct.unpack('I', self._first_block_num)
         else:
-            self.first_block_num = 1
+            self._first_block_num = 1
 
         self.fd.seek(-8, io.SEEK_END)
         pos = self.fd.read(8)
         print(pos)
         pos, = struct.unpack('Q', pos)
-        print(self.version, self.first_block_num, pos)
+        print(self.version, self._first_block_num, pos)
 
         self.index_fd.seek(-8, io.SEEK_END)
         pos = self.index_fd.read(8)
         pos, = struct.unpack('Q', pos)
         print(pos)
 
-        self.fd.seek(pos)
-        raw_block = self.fd.read(1024*1024)
-        block = unpack_native_object(7, raw_block)
-        print(block)
+        # self.fd.seek(pos)
+        # raw_block = self.fd.read(1024*1024)
+        # block = unpack_native_object(7, raw_block)
+        # print(block)
 
         self.index_fd.seek(-16, io.SEEK_END)
         pos = self.index_fd.read(8)
@@ -53,7 +54,6 @@ class BlockLog(object):
         pos, = struct.unpack('Q', pos)
         print(pos)
 
-
     def get_block_count(self):
         print(self.block_log_index_file)
         size = os.path.getsize(self.block_log_index_file)
@@ -61,12 +61,39 @@ class BlockLog(object):
         return size // 8
 
     def get_first_block_num(self):
-        return self.first_block_num
+        return self._first_block_num
+
+    @property
+    def first_block_num(self):
+        return self._first_block_num
 
     def get_head_block_num(self):
-        return self.first_block_num + os.path.getsize(self.block_log_index_file)//8 - 1
+        return self._first_block_num + os.path.getsize(self.block_log_index_file)//8 - 1
 
-if __name__ == '__main__':
-    log = BlockLog('../dd-eos/blocks')
-    # count = log.get_block_count()
-    print(log.get_first_block_num(), log.get_head_block_num())
+    @property
+    def head_block_num(self):
+        return get_head_block_num()
+
+    def read_block_by_id(self):
+        pass
+
+    def read_block_by_num(self, num):
+        index = num - self._first_block_num
+        self.index_fd.seek(index*8)
+        index_data = self.index_fd.read(16)
+        pos, next_pos = struct.unpack('QQ', index_data)
+        block_size = next_pos - pos
+        self.fd.seek(pos)
+        raw_block = self.fd.read(block_size)
+        block = unpack_native_object(7, raw_block)
+        if block:
+            return json.loads(block)
+
+    def read_block_by_id(self, id):
+        previous = id[:8]
+        previous = bytes.fromhex(previous)
+        previous = int.from_bytes(previous, 'big')
+        return self.read_block_by_num(previous + 1)
+
+    def extract_genesis_state(self):
+        pass
