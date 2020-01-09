26863525ae18a5d610c0bf456602829e2c1a4938
learnforpractice <learnforpractice@gmail.com>
2020-01-09 15:54:34 +0800

    Update tests

--- programs/uuos/tests/tester.py
+++ programs/uuos/tests/tester.py
@@ -9,13 +9,15 @@ import asyncio
 import shutil
 import tempfile
 import unittest
-from datetime import datetime, timedelta
 
 test_dir = os.path.dirname(__file__)
 sys.path.append(os.path.join(test_dir, '..'))
 
+from datetime import datetime, timedelta
 from uuos.jsonobject import JsonObject
-from uuos.config import Config, default_config
+from uuos.nativeobject import SignedBlockMessage, PackedTransactionMessage
+
+from uuos import config
 
 from uuos.chain import Chain
 
@@ -27,6 +29,10 @@ gc.set_debug(gc.DEBUG_STATS)
 
 logger = application.get_logger(__name__)
 
+def isoformat(dt):
+    return dt.isoformat(timespec='milliseconds')
+
+#logger = LogWrapper(logger)
 
 genesis_uuos = {
   "initial_timestamp": "2019-10-24T00:00:00.888",
@@ -83,52 +89,52 @@ class ChainTest(object):
 
     def __init__(self):
         logger.info('+++++++++init+++++++++++')
-        config = Object()
-        config.data_dir = tempfile.mkdtemp()
-        config.config_dir = tempfile.mkdtemp()
+        options = Object()
+        options.data_dir = tempfile.mkdtemp()
+        options.config_dir = tempfile.mkdtemp()
 
-        print(config.data_dir, config.config_dir)
+        print(options.data_dir, options.config_dir)
 
-        config.chain_state_db_size_mb = 50
-        config.contracts_console = True
-        config.uuos_mainnet = False
-        config.network = 'test'
-        config.snapshot = ''
+        options.chain_state_db_size_mb = 50
+        options.contracts_console = True
+        options.uuos_mainnet = False
+        options.network = 'test'
+        options.snapshot = ''
 
         self._chain = None
 
-        chain_cfg = ControllerConfig(default_config)
-        chain_cfg.contracts_console = config.contracts_console
-        chain_cfg.blocks_dir = os.path.join(config.data_dir, 'blocks')
-        chain_cfg.state_dir = os.path.join(config.data_dir, 'state')
+        chain_cfg = ControllerConfig(config.default_config)
+        chain_cfg.contracts_console = options.contracts_console
+        chain_cfg.blocks_dir = os.path.join(options.data_dir, 'blocks')
+        chain_cfg.state_dir = os.path.join(options.data_dir, 'state')
 #
         chain_cfg.state_guard_size = 5*1024*1024
         chain_cfg.reversible_cache_size = 50*1024*1024
         chain_cfg.reversible_guard_size = 5*1024*1024
 
 
-        uuos.set_default_data_dir(config.data_dir)
-        uuos.set_default_config_dir(config.config_dir)
+        uuos.set_default_data_dir(options.data_dir)
+        uuos.set_default_config_dir(options.config_dir)
 
-        chain_cfg.state_size = config.chain_state_db_size_mb * 1024 * 1024
+        chain_cfg.state_size = options.chain_state_db_size_mb * 1024 * 1024
 
-        print('config.uuos_mainnet', config.uuos_mainnet)
+        print('config.uuos_mainnet', options.uuos_mainnet)
         chain_cfg.uuos_mainnet = False
 
-        self.config = config
-        if self.config.network == 'uuos':
+        self.options = options
+        if self.options.network == 'uuos':
             chain_cfg.uuos_mainnet = True
             chain_cfg.genesis = genesis_uuos
-        elif self.config.network == 'eos':
+        elif self.options.network == 'eos':
             chain_cfg.genesis = genesis_eos
-        elif self.config.network == 'test':
+        elif self.options.network == 'test':
             pass
         else:
             raise Exception('unknown network')
 
         chain_cfg = chain_cfg.dumps()
         logger.info(chain_cfg)
-        self._chain = Chain(chain_cfg, config.config_dir, config.snapshot)
+        self._chain = Chain(chain_cfg, options.config_dir, options.snapshot)
 
         self.init()
 
@@ -141,11 +147,50 @@ class ChainTest(object):
         return self._chain
 
     def on_accepted_block(self, block, num, block_id):
-        pass
+        msg = SignedBlockMessage.unpack(block)
+        print(msg)
+
+    def calc_pending_block_time(self):
+#        self.chain.abort_block()
+        now = datetime.utcnow()
+        base = self.chain.head_block_time()
+        print(isoformat(base), isoformat(now))
+        if base < now:
+            base = now
+        min_time_to_next_block = config.block_interval_us - int(base.timestamp()*1e6) % config.block_interval_us
+        print('min_time_to_next_block:', min_time_to_next_block)
+        logger.info(f"+++++++++base: {isoformat(base)}")
+        block_time = base + timedelta(microseconds=min_time_to_next_block)
+        logger.info(f"+++block_time: {isoformat(block_time)}")
+        if block_time - now < timedelta(microseconds=config.block_interval_us/10):
+            block_time += timedelta(microseconds=config.block_interval_us)        
+        logger.info(f"+++block_time: {isoformat(block_time)}")
+        return block_time
+
+    #   permission_level
+    #   account_name    actor;
+    #   permission_name permission;
+
+    #   account_name               account;
+    #   action_name                name;
+    #   vector<permission_level>   authorization;
+    #   bytes                      data;
+
 
     def start_block(self):
-        self.chain.abort_block()
-        self.chain.start_block(datetime.utcnow().isoformat())
+        for i in range(10):
+            self.chain.abort_block()
+            self.chain.start_block(isoformat(self.calc_pending_block_time()))
+            trx = self.gen_trx()
+            deadline = datetime.utcnow() + timedelta(microseconds=30000)
+            billed_cpu_time_us = 2000
+            ret = self.chain.push_transaction(trx, isoformat(deadline), billed_cpu_time_us)
+            print(ret)
+            trxs = self.chain.get_unapplied_transactions()
+            print(trxs)
+            self.chain.finalize_block('5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3')
+            self.chain.commit_block()
+            print('head_block_num:', self.chain.head_block_num())
 
     def producer_block(self, skip_time):
         if isinstance(skip_time, int):
@@ -177,10 +222,36 @@ class ChainTest(object):
         self.start_block()
         # self.producer_block(skip_time)
 
+    def test2(self):
+        self.calc_pending_block_time()
+
+    def gen_trx(self):
+        chain_id = self.chain.id()
+        ref_block_id = self.chain.last_irreversible_block_id()
+        priv_key = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+        actions = []
+        action = {
+            'account':'eosio',
+            'name':'sayhello',
+            'data':'aabbcc',
+            'authorization':[{'actor':'eosio', 'permission':'active'}]
+        }
+        actions.append(action)
+        actions = json.dumps(actions)
+        expiration = datetime.utcnow() + timedelta(seconds=60)
+        expiration = isoformat(expiration)
+        r = self.chain.gen_transaction(actions, expiration, ref_block_id, chain_id, False, priv_key)
+        print(r)
+        return r
+
+        print(r)
+        r = PackedTransactionMessage.unpack(r)
+        print(r)
+
     def free(self):
         self.chain.free()
-        shutil.rmtree(self.config.config_dir)
-        shutil.rmtree(self.config.data_dir)
+        shutil.rmtree(self.options.config_dir)
+        shutil.rmtree(self.options.data_dir)
 
 class UUOSTester(unittest.TestCase):
     def __init__(self, testName, extra_args=[]):
@@ -192,6 +263,12 @@ class UUOSTester(unittest.TestCase):
     def test1(self):
         self.chain.test1()
         # datetime.datetime.utcnow().isoformat()
+
+    def test2(self):
+        self.chain.test2()
+
+    def test3(self):
+        self.chain.test3()
 
     @classmethod
     def setUpClass(cls):
