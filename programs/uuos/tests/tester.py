4b30d39607abbf69e2569ef42da76106857aff07
learnforpractice <learnforpractice@gmail.com>
2020-01-10 21:52:45 +0800

    Update test

--- programs/uuos/tests/tester.py
+++ programs/uuos/tests/tester.py
@@ -576,108 +576,32 @@ def apply(receiver, code, action):
         self.produce_block()
 
     def test7(self):
-        for i in range(10):
-            for j in range(40):
+        for i in range(2):
+            for j in range(10):
                 args = int.to_bytes(j, 4, 'little')
                 r = self.push_action('helloworld11', 'sayhello', args, 'helloworld11')
                 r = self.push_action('alice', 'sayhello', args)
             self.produce_block()
 
-    def db_test1(self):
-        logger.info('+++++++++++++++db_test1++++++++++++++')
-        db_test = os.path.join(test_dir, 'test_contracts/db_test.py')
+    def read_code(self, code_file):
+        db_test = os.path.join(test_dir, 'test_contracts', code_file)
         with open(db_test, 'r') as f:
             code = f.read()
         code = compile(code, "contract", 'exec')
         code = marshal.dumps(code)
+        return code
+
+    def db_test1(self):
+        logger.info('+++++++++++++++db_test1++++++++++++++')
+        code = self.read_code('db_test.py')
         self.deploy_contract('helloworld11', code, b'', 1)
         r = self.push_action('helloworld11', 'sayhello', b'', 'helloworld11')
         r = JsonObject(r)
 #        print(r)
         self.produce_block()
 
-
     def db_test2(self):
-        code = '''
-import struct
-import db
-
-class MyData(object):
-    def __init__(self, a: int, b: int, c: int, d: float):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-
-    def pack(self):
-        b = int.to_bytes(self.b, 16, 'little')
-        c = int.to_bytes(self.c, 32, 'little')
-        return struct.pack('Q16s32sd', self.a, b, c, self.d)
-
-    @classmethod
-    def unpack(cls, data):
-        a, b, c, d = struct.unpack('Q16s32sd', data)
-        b = int.from_bytes(b, 'little')
-        c = int.from_bytes(c, 'little')
-        return MyData(a, b, c, d)
-
-    @property
-    def primary_key(self):
-        return self.a
-
-    # @x.setter
-    # def primary_key(self, a):
-    #     pass
-
-    def get_primary_key(self):
-        return self.a
-
-    def get_secondary_values(self):
-        return (self.a, self.b, self.c, self.d)
-
-    def set_secondary_value(self, idx):
-        if idx == 0:
-            pass
-        elif idx == 1:
-            pass
-        elif idx == 2:
-            pass
-        elif idx == 3:
-            pass
-
-    @classmethod
-    def get_secondary_indexes(self):
-        return (db.idx64, db.idx128, db.idx256, db.idx_double)
-
-def apply(receiver, code, action):
-    payer = receiver
-    code = N('helloworld11')
-    scope = N('scopee')
-    table = N('tableee')
-    mi = db.MultiIndex(code, scope, table, MyData)
-
-    if action == N('store'):
-        primary_key = 112
-        d = MyData(primary_key, 2, 3, 5.0)
-        d.payer = receiver
-        mi.store(d)
-    elif action == N('get'):
-        itr = mi.find(112)
-        print(itr)
-        data = mi.get(itr)
-        print(data.a, data.b, data.c, data.d)
-    elif action == N('update'):
-        itr = mi.find(112)
-        print(itr)
-        data = mi.get(itr)
-        print('+++b:', data.b)
-        data.payer = payer
-        data.b += 1
-        mi.store(data)
-'''
-
-        code = compile(code, "contract", 'exec')
-        code = marshal.dumps(code)
+        code = self.read_code('db_test2.py')
         self.deploy_contract('helloworld11', code, b'', 1)
         r = self.push_action('helloworld11', 'store', b'')
         r = JsonObject(r)
@@ -693,83 +617,7 @@ def apply(receiver, code, action):
         self.produce_block()
 
     def db_test3(self):
-        code = '''
-import struct
-import db
-
-class MyData(object):
-    def __init__(self, a: int, b: int, c: int, d: float):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-
-    def pack(self):
-        b = int.to_bytes(self.b, 16, 'little')
-        c = int.to_bytes(self.c, 32, 'little')
-        return struct.pack('Q16s32sd', self.a, b, c, self.d)
-
-    @classmethod
-    def unpack(cls, data):
-        a, b, c, d = struct.unpack('Q16s32sd', data)
-        b = int.from_bytes(b, 'little')
-        c = int.from_bytes(c, 'little')
-        return MyData(a, b, c, d)
-
-    @property
-    def primary_key(self):
-        return self.a
-
-    # @x.setter
-    # def primary_key(self, a):
-    #     pass
-
-    def get_primary_key(self):
-        return self.a
-
-    def get_secondary_values(self):
-        return ()
-
-    def set_secondary_value(self, idx):
-        if idx == 0:
-            pass
-        elif idx == 1:
-            pass
-        elif idx == 2:
-            pass
-        elif idx == 3:
-            pass
-
-    @classmethod
-    def get_secondary_indexes(self):
-        return ()
-
-code = N('helloworld11')
-scope = N('scopee')
-table = N('tableee')
-mi = db.MultiIndex(code, scope, table, MyData)
-
-def apply(receiver, code, action):
-    payer = receiver
-    if action == N('store'):
-        primary_key = 112
-        d = MyData(primary_key, 2, 3, 5.0)
-        d.payer = receiver
-        mi.store(d)
-    elif action == N('get'):
-        itr = mi.find(112)
-#        print(itr)
-        data = mi.get(itr)
-#        print(data.a, data.b, data.c, data.d)
-    elif action == N('update'):
-        itr = mi.find(112)
-        print(itr)
-        data = mi.get(itr)
-        print('+++b:', data.b)
-        data.payer = payer
-        data.b += 1
-        mi.store(data)
-'''
+        code = self.read_code('db_test3.py')
 
         code = compile(code, "contract", 'exec')
         code = marshal.dumps(code)
