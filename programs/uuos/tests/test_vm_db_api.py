eaed02463cc1ce6a2e3a99c46169743026bec413
learnforpractice <learnforpractice@gmail.com>
2020-01-15 15:57:44 +0800

    test case for vm db api

---
+++ programs/uuos/tests/test_vm_db_api.py
@@ -0,0 +1,93 @@
+import os
+import sys
+import gc
+import unittest
+
+test_dir = os.path.dirname(__file__)
+sys.path.append(os.path.join(test_dir, '..'))
+
+from uuos import application
+from chaintest import ChainTest
+
+logger = application.get_logger(__name__)
+
+class DBTest(ChainTest):
+
+    def test_vm_api(self):
+        code = '''
+import db
+def apply(receiver, code, action):
+    code = receiver
+    scope = 'scope'
+    table = 'table'
+    payer = receiver
+    id = 'hello'
+    data = read_action_data()
+    if action == N('destroy'):
+        while True:
+            itr = db.end_i64(code, scope, table)
+            if itr == -1:
+                break
+            itr, primary = db.previous_i64(itr)
+            if itr < 0:
+                break
+            db.remove_i64(itr)
+        print('destroy done!')
+    elif action == N('init'):
+        db.store_i64(scope, table, payer, 1, b'1')
+        db.store_i64(scope, table, payer, 3, b'3')
+        db.store_i64(scope, table, payer, 5, b'5')
+        db.store_i64(scope, table, payer, 7, b'7')
+        print('init done!')
+    elif action == N('test'):
+        itr = db.upperbound_i64(code, scope, table, 3)
+        data = db.get_i64(itr)
+        print('upperbound of 3:', data)
+
+        itr = db.upperbound_i64(code, scope, table, 2)
+        data = db.get_i64(itr)
+        print('upperbound of 2:', data)
+
+        itr = db.lowerbound_i64(code, scope, table, 3)
+        data = db.get_i64(itr)
+        print('lowerbound of 3:', data)
+
+        itr = db.lowerbound_i64(code, scope, table, 2)
+        data = db.get_i64(itr)
+        print('lowerbound of 2:', data)
+        '''
+        name = 'helloworld11'
+        code = self.compile_py_code(code)
+        self.deploy_contract(name, code, b'', 1)
+        r = self.push_action(name, 'destroy', b'')
+        r = self.push_action(name, 'init', b'hello,world')
+        self.produce_block()
+
+class DBTesterCase(unittest.TestCase):
+    def __init__(self, testName):
+        logger.info('+++++++++++++++++++++DBTesterCase++++++++++++++++')
+        super(DBTesterCase, self).__init__(testName)
+
+    def test_vm_api(self):
+        logger.info('+++++++++++++db_test1+++++++++++++++')
+        DBTesterCase.chain.test_vm_api()
+
+    @classmethod
+    def setUpClass(cls):
+        logger.info('++++++++++++setUpClass++++++++++++')
+        cls.chain = DBTest()
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.chain:
+            cls.chain.free()
+            cls.chain = None
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
