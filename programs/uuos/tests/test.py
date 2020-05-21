fd915752978d5cc93b8a734aefd043ed606e1513
learnforpractice <learnforpractice@gmail.com>
2020-05-21 12:25:29 +0800

    Update test

--- programs/uuos/tests/test.py
+++ programs/uuos/tests/test.py
@@ -13,12 +13,22 @@ class Test(object):
     def setup_class(cls):
         cls.chain = ChainTest(uuos_network=False)
 
-    def test_create_account(self):
-        self.chain.test_create_account()
-
     @classmethod
     def teardown_class(cls):
         cls.chain.free()
+
+    def setup_method(self, method):
+        logger.info("starting execution of tc: {}".format(method.__name__))
+
+    def teardown_method(self, method):
+        logger.info("Ending execution of tc: {}".format(method.__name__))
+
+    def test_create_account(self):
+        self.chain.test_create_account()
+
+    def test_hello_world(self):
+        logger.info('hello,world')
+
 
 #./uuos/uuos -m pytest ../../programs/uuos/tests/test.py::Test::test_create_account
 
