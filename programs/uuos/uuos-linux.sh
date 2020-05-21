23069897b3976a7c546c23ba9b28a8c2f11bce92
learnforpractice <learnforpractice@gmail.com>
2020-05-21 18:00:49 +0800

    Updates

---
+++ programs/uuos/uuos-linux.sh
@@ -0,0 +1,7 @@
+export PROJECT_DIR="/home/newworld/dev/uuos3"
+export CHAIN_API_SHARED_LIB=$PROJECT_DIR/build//libraries/chain_api/libchain_api.so
+export UUOS_EXT_LIB=$PROJECT_DIR/programs/pyeos
+export PYTHONPATH=$UUOS_EXT_LIB:$PROJECT_DIR/programs/uuos:'/usr/lib/python37.zip':'/usr/lib/python3.7':'/usr/lib/python3.7/lib-dynload':'/home/newworld/.local/lib/python3.7/site-packages':'/usr/local/lib/python3.7/dist-packages':'/usr/lib/python3/dist-packages'
+
+export PYTHONHOME="/usr/lib/python3.7"
+export PYTHON_SHARED_LIB_PATH="/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu/libpython3.7.so"
--- programs/uuos/uuos-mac.sh
+++ programs/uuos/uuos-mac.sh
@@ -1,7 +1,7 @@
 export PROJECT_DIR="/Users/newworld/dev/uuos3"
 export CHAIN_API_SHARED_LIB=$PROJECT_DIR/build//libraries/chain_api/libchain_api.dylib
 export UUOS_EXT_LIB=$PROJECT_DIR/programs/pyeos
-export PYTHONPATH=$PROJECT_DIR/programs/uuos
+export PYTHONPATH=$PROJECT_DIR/programs/uuos:$UUOS_EXT_LIB
 
 export PYTHONHOME="/Users/newworld/opt/anaconda3"
 export PYTHON_SHARED_LIB_PATH="/Users/newworld/opt/anaconda3/lib/libpython3.7m.dylib"
