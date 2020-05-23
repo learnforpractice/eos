08399655db8dafea4752ab92496f906afb87c8d1
learnforpractice <learnforpractice@gmail.com>
2020-05-23 20:55:09 +0800

    Update db test

--- programs/uuos/tests/pytest/test_db_api.py
+++ programs/uuos/tests/pytest/test_db_api.py
@@ -18,9 +18,6 @@ class Test(object):
     def setup_class(cls):
         cls.main_token = 'UUOS'
         cls.chain = chain = ChainTest(uuos_network=True)
-        contract_name = 'helloworld11'
-        code = chain.compile_py_code_from_file('db_api_test.py')
-        chain.deploy_contract(contract_name, code, b'', vmtype=1)
 
     @classmethod
     def teardown_class(cls):
@@ -32,8 +29,26 @@ class Test(object):
     def teardown_method(self, method):
         logger.info("Ending execution of tc: {}".format(method.__name__))
 
-    def test_vm_api(self):
+    def test_db_i64(self):
         name = 'helloworld11'
+        code = self.chain.compile_py_code_from_file('db_test/db_test_i64.py')
+        self.chain.deploy_contract(name, code, b'', vmtype=1)
+
+        r = self.chain.push_action(name, 'destroy', b'')
+        r = self.chain.push_action(name, 'init', b'hello,world')
+        self.chain.push_action(name, 'test', b'')
+        self.chain.push_action(name, 'test1', b'')
+        self.chain.push_action(name, 'test2', b'')
+        self.chain.produce_block()
+
+        r = self.chain.push_action(name, 'destroy', b'')
+        self.chain.produce_block()
+
+    def test_db_idx64(self):
+        name = 'helloworld11'
+        code = self.chain.compile_py_code_from_file('db_test/db_test_idx64.py')
+        self.chain.deploy_contract(name, code, b'', vmtype=1)
+
         r = self.chain.push_action(name, 'destroy', b'')
         r = self.chain.push_action(name, 'init', b'hello,world')
         self.chain.push_action(name, 'test', b'')
