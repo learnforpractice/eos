453f9ace6bb6b9750663fb3d92913805bdff7d8d
learnforpractice <learnforpractice@gmail.com>
2020-05-24 14:11:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,13 +8,17 @@ fi
 
 
 function build_project() {
+	local ret=0
 	if [ -d "$(pwd)/build" ]; then
         	pushd build
         	make -j$NPROC
+			ret=$?
 	        popd
 	else
 		./scripts/eosio_build.sh
+		ret=$?
 	fi
+	return $ret
 }
 
 function build_pyeos() {
