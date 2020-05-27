25cd4c666da41b1d38e0efdeebb039084baac80b
learnforpractice <learnforpractice@gmail.com>
2020-05-27 23:03:49 +0800

    Optimizing apply

--- contracts/python37/libpython37/pythoninit.c
+++ contracts/python37/libpython37/pythoninit.c
@@ -21,8 +21,7 @@ int get_code(uint64_t account, char *code, size_t size);
 
 static int initialized = 0;
 static int g_counter = 0;
-static void *current_module;
-
+static PyObject *g_apply = NULL;
 
 PyObject* PyInit_spam(void);
 PyMODINIT_FUNC PyInit__sha1(void);
@@ -51,7 +50,7 @@ void python_init(void) {
     }
 }
 
-void *python_load_module(const char *code, int size) {
+PyObject *python_load_module(const char *code, int size) {
     PyObject *contract = PyUnicode_FromString("contract");
     PyObject *m = PyImport_ImportModuleObject(contract, code, size);
     if (PyErr_Occurred()) {
@@ -82,6 +81,25 @@ void python_call_module(void *m, uint64_t receiver, uint64_t code, uint64_t acti
     Py_DECREF(args);
 }
 
+void python_call_apply(uint64_t receiver, uint64_t code, uint64_t action) {
+    PyObject *args[3];
+    if (g_apply == NULL) {
+        eosio_assert(0, "apply not found!");
+        return;
+    }
+    args[0] = PyLong_FromUnsignedLongLong(receiver);
+    args[1] = PyLong_FromUnsignedLongLong(code);
+    args[2] = PyLong_FromUnsignedLongLong(action);
+
+    _PyObject_FastCall(g_apply, args, 3);
+
+    if (PyErr_Occurred()) {
+        PyErr_PrintEx(0);
+        eosio_assert(0, "apply exit with exceptions!");
+    }
+    Py_DECREF(args);
+}
+
 void check_error(void) {
     if (PyErr_Occurred()) {
         PyErr_Print();
@@ -90,10 +108,11 @@ void check_error(void) {
 }
 
 void python_vm_apply( uint64_t receiver, uint64_t code, uint64_t action ) {
-    if (!current_module) {
+    if (g_apply == NULL) {
+        eosio_assert(0, "apply not found!");
         return;
     }
-    python_call_module(current_module, receiver, code, action);
+    python_call_apply(receiver, code, action);
 }
 
 void python_vm_call( uint64_t func_name, uint64_t receiver, uint64_t code, uint64_t action ) {
@@ -111,7 +130,13 @@ void python_vm_call( uint64_t func_name, uint64_t receiver, uint64_t code, uint6
 //        prints("++++++++++++code buffer ");printi(str_code);prints("\n");
 //        receiver = current_receiver();
         int size = get_code(receiver, str_code, code_size);
-        current_module = python_load_module(str_code, code_size);
+        PyObject *m = python_load_module(str_code, code_size);
+        PyObject *d = PyModule_GetDict(m);
+        g_apply = PyDict_GetItemString(d, "apply");
+        if (g_apply == NULL) {
+            eosio_assert(0, "apply not found!");
+            return;
+        }
 //        printf("+++++++++++++++get_current_memory: %d\n", get_current_memory());
         memset(str_code, 0, code_size);
         end = (uint32_t)get_current_memory();
