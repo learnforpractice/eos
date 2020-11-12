d956ffd2ba72476d9b68fea15e543e3e26adab7f
learnforpractice <learnforpractice@gmail.com>
2020-11-13 00:36:22 +0800

    Update micropython path

--- libraries/vm/vm_micropython/gen.sh
+++ libraries/vm/vm_micropython/gen.sh
@@ -1,8 +1,10 @@
 mkdir tmp
 pushd tmp
-~/dev/wasm/wabt/build/wasm2c -o micropython.c ../../../../build/externals/micropython/micropython
+~/dev/wasm/wabt/build/wasm2c -o micropython.c ../../../../build/externals/micropython/ports/uuosio/micropython
 popd
 mv ./tmp/micropython.h micropython.h
 mv ./tmp/micropython.c micropython.c.bin
 python3 patch.py
 rm -r tmp
+
+ 
