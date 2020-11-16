cdf7a8cca8cfd9b729a2300a79586984b9c8c409
learnforpractice <learnforpractice@gmail.com>
2020-11-16 11:30:22 +0800

    Execute code only when receiver == code

--- programs/uuos/tests/test_contracts/test_call_contract.cpp
+++ programs/uuos/tests/test_contracts/test_call_contract.cpp
@@ -10,14 +10,16 @@ extern "C" {
     int call_contract_set_results(void* result, size_t size1);
 
     void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
-         uint64_t args[2];
-         int args_size = ::call_contract_get_args(&args, sizeof(args));
-         eosio::print("+++++++++++call: arg count:", args_size, "\n");
-         eosio::check(args_size == 16, "bad args size");
-         if (args[0] == eosio::name("calltest1").value) {
-            eosio::print("+++++++++++call: args[1]:", args[1], "\n");
-            args[1] += 1;
-            ::call_contract_set_results(&args[1], sizeof(uint64_t));
-         }
+        if (receiver == code) {
+            uint64_t args[2];
+            int args_size = ::call_contract_get_args(&args, sizeof(args));
+            eosio::print("+++++++++++call: arg count:", args_size, "\n");
+            eosio::check(args_size == 16, "bad args size");
+            if (args[0] == eosio::name("calltest1").value) {
+                eosio::print("+++++++++++call: args[1]:", args[1], "\n");
+                args[1] += 1;
+                ::call_contract_set_results(&args[1], sizeof(uint64_t));
+            }
+        }
     }
 }
