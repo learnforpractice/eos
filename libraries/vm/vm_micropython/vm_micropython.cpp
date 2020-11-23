e3b7359bfe15db9f6e4bd2fa00103d4be5097225
learnforpractice <learnforpractice@gmail.com>
2020-11-23 20:45:37 +0800

    10%+ jump stack performance improvement by reduce memory copy

--- libraries/vm/vm_micropython/vm_micropython.cpp
+++ libraries/vm/vm_micropython/vm_micropython.cpp
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <wasm-rt-impl.h>
 #include <setjmp.h>
-#include <vector>
 #include <vm_api.h>
+
+#include <vector>
+#include <array>
 
 extern "C" {
   int micropython_contract_init(int type, const char *py_src, size_t size);
@@ -22,7 +24,7 @@ extern "C" int vm_apply(uint64_t receiver, uint64_t code, uint64_t action) {
   }
 }
 
-static std::vector<std::vector<uint8_t>> setjmp_stack;
+//static std::vector<std::vector<uint8_t>> setjmp_stack;
 
 extern "C" void print_hex(char *data, size_t size) {
   for (int i=0;i<size;i++) {
@@ -30,6 +32,42 @@ extern "C" void print_hex(char *data, size_t size) {
   }
   printf("\n");
 }
+
+class JumpStack {
+  private:
+    std::vector<std::array<uint8_t, sizeof(jmp_buf)>> jmp_stack;
+    int top;
+    int max_stack;
+
+  public:
+    JumpStack(size_t _max_stack) {
+      max_stack = _max_stack;
+      jmp_stack.resize(_max_stack);
+      top = -1;
+    }
+
+    void push_back(jmp_buf buf) {
+      get_vm_api()->eosio_assert(top < max_stack, "stack overflow!");
+      top += 1;
+      memcpy(jmp_stack[top].data(), buf, sizeof(jmp_buf));
+    }
+
+    void pop_back() {
+      get_vm_api()->eosio_assert(top >= 0, "stack underflow!");
+      top -= 1;
+    }
+
+    std::array<uint8_t, sizeof(jmp_buf)>& back() {
+      get_vm_api()->eosio_assert(top >= 0, "empty stack!");
+      return jmp_stack[top];
+    }
+
+    void clear() {
+      top = -1;
+    }
+};
+
+static JumpStack setjmp_stack(50);
 
 extern "C" {
 #include <stacktrace.h>
@@ -42,27 +80,16 @@ void setjmp_clear_stack() {
 }
 
 void setjmp_push(jmp_buf buf) {
-  std::vector<uint8_t> _buf(sizeof(jmp_buf));
-  memcpy(_buf.data(), buf, sizeof(jmp_buf));
-  setjmp_stack.push_back(_buf);
+  setjmp_stack.push_back(buf);
 }
 
 void setjmp_pop(jmp_buf buf) {
-  if (setjmp_stack.empty()) {
-    get_vm_api()->eosio_assert(false, "setjmp stack empty!");
-    return;
-  }
-
-  std::vector<uint8_t>& _buf = setjmp_stack.back();
+  std::array<uint8_t, sizeof(jmp_buf)>& _buf = setjmp_stack.back();
   memcpy(buf, _buf.data(), sizeof(jmp_buf));
   setjmp_stack.pop_back();
 }
 
 void setjmp_discard_top() {
-  if (setjmp_stack.empty()) {
-    get_vm_api()->eosio_assert(false, "setjmp stack empty!");
-    return;
-  }
   setjmp_stack.pop_back();
 }
 
