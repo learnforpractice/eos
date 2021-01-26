de799db86955cfbc6a9fbb8f20c561fc71be91d2
learnforpractice <learnforpractice@gmail.com>
2021-01-26 19:40:27 +0800

    Add chain_proxy & apply_context_proxy

---
+++ libraries/chain/eosio_chain.cpp
@@ -0,0 +1,25 @@
+#include <eosio/chain/wasm_interface.hpp>
+#include <eosio/chain/chain_proxy.hpp>
+
+using namespace eosio::chain;
+
+//libraries/chain/webassembly/eos-vm-oc/compile_monitor.cpp
+extern "C" void start_compile_monitor();
+
+extern "C" {
+   eosio::chain::wasm_interface* eos_vm_interface_init2(int vmtype, bool tierup, eosio::chain::chain_proxy& api) {
+#ifdef EOSIO_EOS_VM_OC_RUNTIME_ENABLED
+      if (tierup) {
+         static bool init = false;
+         if (!init) {
+            init = true;
+            start_compile_monitor();
+         }
+      }
+#endif
+//      bool tierup = api.conf.eosvmoc_tierup;
+      wasm_interface* interface = new wasm_interface((wasm_interface::vm_type)vmtype, tierup, api.db(), api.state_dir(), api.conf.eosvmoc_config);
+//      wasm_interface* interface = new wasm_interface(wasm_interface::vm_type::wabt, tierup, api.db(), api.state_dir(), api.conf.eosvmoc_config, api);
+      return interface;
+   }
+}
---
+++ libraries/chain/include/eosio/chain/chain_proxy.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <eosio/chain/controller.hpp>
+
+namespace eosio { namespace chain {
+
+class apply_context;
+class wasm_interface;
+
+class chain_proxy {
+
+public:
+    chain_proxy(const controller::config& conf, controller& ctrl);
+    virtual void set_context(apply_context* ctx);
+    virtual apply_context& get_context();
+
+    virtual const chainbase::database& db();
+    virtual const boost::filesystem::path state_dir();
+    virtual bool get_code_by_code_hash(const digest_type& code_hash, const uint8_t vm_type, const uint8_t vm_version, const char** code, size_t* size, uint32_t* first_block_used);
+
+    virtual void eos_vm_interface_apply(const digest_type& code_hash, const uint8_t vm_type, const uint8_t vm_version, eosio::chain::apply_context& context);
+    virtual void eos_vm_micropython_apply(const digest_type& code_hash, const uint8_t vm_type, const uint8_t vm_version, eosio::chain::apply_context& context);
+    virtual bool contracts_console();
+public:
+    controller::config conf;
+
+private:
+    controller &c;
+    apply_context* ctx = nullptr;
+    wasm_interface *eos_vm_interface;
+    wasm_interface *eos_vm_micropython;
+};
+
+} } //eosio::chain
