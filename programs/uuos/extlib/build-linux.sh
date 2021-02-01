71c75026a0a66e1ec699a75c86499762ccc16778
learnforpractice <learnforpractice@gmail.com>
2021-02-02 02:09:59 +0800

    Fix bash scripts for python extention lib

--- programs/uuos/extlib/build-linux.sh
+++ programs/uuos/extlib/build-linux.sh
@@ -3,4 +3,3 @@ CC=clang CXX=clang++ python3.7 setup.py sdist bdist_wheel
 #install_name_tool -add_rpath  /Users/newworld/dev/uuos2/build/programs/nodeos ./_skbuild/macosx-10.9-x86_64-3.7/cmake-install/uuos/_uuos.cpython-37m-darwin.so
 #cp ./_skbuild/macosx-10.9-x86_64-3.7/cmake-install/uuos/_uuos.cpython-37m-darwin.so .
 cp ./_skbuild/linux-x86_64-3.7/cmake-build/src/_uuos.cpython-37m-x86_64-linux-gnu.so .
-
