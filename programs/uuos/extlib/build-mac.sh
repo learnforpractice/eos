22ebd1fba05ec909382e9d00cbb676668bf1d02d
learnforpractice <learnforpractice@gmail.com>
2021-02-02 11:30:14 +0800

    Cleanup files

--- programs/uuos/extlib/build-mac.sh
+++ programs/uuos/extlib/build-mac.sh
@@ -1,5 +1,2 @@
 CC=clang CXX=clang++ python3.7 setup.py sdist bdist_wheel --plat-name macosx-10.9-x86_64
-#install_name_tool -add_rpath  /Users/newworld/dev/uuos2/build/programs/nodeos ./_skbuild/macosx-10.9-x86_64-3.7/cmake-install/uuos/_uuos.cpython-37m-darwin.so
-#cp ./_skbuild/macosx-10.9-x86_64-3.7/cmake-install/uuos/_uuos.cpython-37m-darwin.so .
-
 cp ./_skbuild/macosx-10.9-x86_64-3.7/cmake-install/uuosio/_uuos.cpython-37m-darwin.so .
