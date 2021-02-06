55b23af4554e7a9ce3a6ec0cc7d064117ebf017e
learnforpractice <learnforpractice@gmail.com>
2021-02-07 02:44:02 +0800

    Update main.py

--- programs/uuos/extlib/uuosio/main.py
+++ programs/uuos/extlib/uuosio/main.py
@@ -1,9 +1,13 @@
 import sys
 from uuosio import uuos
-if __name__ == "__main__":
+
+def run():
     ret = uuos.init()
     print('++++ret:', ret)
     if not ret == 0:
         sys.exit(ret)
     uuos.exec()
     print('done!')
+
+if __name__ == "__main__":
+    run()
