dd345bb89b5fbf3377dce8c23dd3339398f7c389
learnforpractice <learnforpractice@gmail.com>
2021-03-06 11:18:13 +0800

    Add explicit_cpu_bill for push_transaction

--- libraries/chain/chain_api/chain_proxy.cpp
+++ libraries/chain/chain_api/chain_proxy.cpp
@@ -548,7 +548,7 @@ void chain_proxy::gen_transaction(string& _actions, string& expiration, string& 
     } CATCH_AND_LOG_EXCEPTION(this);
 }
 
-string chain_proxy::push_transaction(string& _packed_trx, string& deadline, uint32_t billed_cpu_time_us) {
+string chain_proxy::push_transaction(string& _packed_trx, string& deadline, uint32_t billed_cpu_time_us, bool explicit_cpu_bill) {
     try {
         vector<char> packed_trx(_packed_trx.c_str(), _packed_trx.c_str()+_packed_trx.size());
         auto ptrx = std::make_shared<packed_transaction>();
@@ -557,7 +557,7 @@ string chain_proxy::push_transaction(string& _packed_trx, string& deadline, uint
         auto ptrx_meta = transaction_metadata::recover_keys(ptrx, c->get_chain_id());
     //    auto ptrx_meta = transaction_metadata::create_no_recover_keys( trx, transaction_metadata::trx_type::input );
         auto _deadline = fc::time_point::from_iso_string(deadline);
-        auto ret = c->push_transaction(ptrx_meta, _deadline, billed_cpu_time_us, false);
+        auto ret = c->push_transaction(ptrx_meta, _deadline, billed_cpu_time_us, explicit_cpu_bill);
         return fc::json::to_string(ret, fc::time_point::maximum());
         // if (ret->except) {
         //     return false;
